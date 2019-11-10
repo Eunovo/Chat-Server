0e1c8566fded6946f7acb5ad8fe6592d32afa461
Eunovo <eunovo9@gmail.com>
2019-11-10 22:17:41 +0100

    define Friend entity

---
+++ user-service/src/main/java/com/eunovo/userservice/entities/Friend.java
@@ -0,0 +1,40 @@
+package com.eunovo.userservice.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "source_id", "target_id" }))
+public class Friend implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(optional = false, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "source_id", referencedColumnName = "id")
+    private User source;
+
+    @OneToOne(optional = false, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "target_id", referencedColumnName = "id")
+    private User target;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean accepted = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Friend(User source, User target) {
+        this.source = source;
+        this.target = target;
+    }
+}
