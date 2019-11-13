541764a0a5bcd57533bd1922e978711d46736ee6
Eunovo <eunovo9@gmail.com>
2019-11-13 23:07:18 +0100

    add NoArgsConstructor

--- user-service/src/main/java/com/eunovo/userservice/models/SignupUser.java
+++ user-service/src/main/java/com/eunovo/userservice/models/SignupUser.java
@@ -2,11 +2,11 @@ package com.eunovo.userservice.models;
 
 import com.eunovo.userservice.entities.User;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupUser {
     private String username;
     private String password;
