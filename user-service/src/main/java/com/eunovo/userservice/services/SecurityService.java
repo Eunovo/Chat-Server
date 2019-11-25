8ef45b16c447626dd4d57eafced657db43035051
Eunovo <eunovo9@gmail.com>
2019-11-25 17:47:21 +0100

    implement SecurityService

--- user-service/src/main/java/com/eunovo/userservice/services/SecurityService.java
+++ user-service/src/main/java/com/eunovo/userservice/services/SecurityService.java
@@ -1,7 +1,5 @@
 package com.eunovo.userservice.services;
 
-import com.eunovo.userservice.entities.User;
-
 public interface SecurityService {
-    public User getLoggedInUser();
+    public String getLoggedInUsername();
 }
