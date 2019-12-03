3e4a21a9dc8a18f74de138139672623ad8a0eadd
Eunovo <eunovo9@gmail.com>
2019-12-03 19:14:51 +0100

    create test case for route matching

---
+++ api-gateway/test/rerouter/rerouter.spec.js
@@ -0,0 +1,19 @@
+const mocha = require("mocha");
+const { expect } = require("chai");
+
+const { makeRouter } = require("../../src/rerouter/rerouter");
+
+describe("Rerouter", () => {
+    it("should route the request as specified in the routes input", () => {
+        const target = "http://auth.com";
+        const routes = new Map([
+            ["auth/*", target]
+        ]);
+        const testRequest = {
+            url: "http://test.com/auth"
+        };
+        const route = makeRouter(routes);
+        let newRequest = route(testRequest);
+        expect(newRequest.url).to.equal(target);
+    });
+});
