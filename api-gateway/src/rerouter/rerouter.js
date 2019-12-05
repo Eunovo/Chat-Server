4c0b68e6cc9abfafff7968bd5ddb1c97098ef851
Eunovo <eunovo9@gmail.com>
2019-12-05 19:19:54 +0100

    complete transform url function

--- api-gateway/src/rerouter/rerouter.js
+++ api-gateway/src/rerouter/rerouter.js
@@ -1,27 +1,20 @@
 module.exports.makeRouter = (routes) => {
     return (req) => {
-        let url = transformUrl(req.url);
+        let url = transformUrl(req.url, routes);
         return {
-            url: "http://auth.com/login"
+            url
         };
     };
 }
 
 const transformUrl = (url, routes) => {
-    return "http://auth.com/login";
-}
-
-module.exports.urlMatcher = (matcher, url) => {
-    let indexOfStar = matcher.indexOf("*");
-    if (matcher.endsWith('*')) {
-        let matcherString = matcher.substring(0, indexOfStar);
-        return url.startsWith(matcherString);
-    } else if (matcher.startsWith('*')) {
-        let matcherString = matcher.substr(1);
-        return url.endsWith(matcherString);
-    } else {
-        let preffix = matcher.substring(0, indexOfStar);
-        let suffix = matcher.substr(indexOfStar + 1);
-        return url.startsWith(preffix) && url.endsWith(suffix);
-    }
+    let transformedUrl = "";
+    routes.forEach((value, key) => {
+        let index = url.indexOf(key);
+        if (index !== -1) {
+            transformedUrl = value + url.substr(
+                index + key.length - 1);
+        }
+    });
+    return transformedUrl;
 }
--- api-gateway/test/rerouter/rerouter.spec.js
+++ api-gateway/test/rerouter/rerouter.spec.js
@@ -1,13 +1,13 @@
 const mocha = require("mocha");
 const { expect } = require("chai");
 
-const { makeRouter, urlMatcher } = require("../../src/rerouter/rerouter");
+const { makeRouter } = require("../../src/rerouter/rerouter");
 
 describe("Rerouter", () => {
     it("should route the request as specified in the routes input", () => {
         const target = "http://auth.com";
         const routes = new Map([
-            ["auth/*", target]
+            ["auth/", target]
         ]);
         const path = "/login";
         const testRequest = {
@@ -16,31 +16,5 @@ describe("Rerouter", () => {
         const route = makeRouter(routes);
         let newRequest = route(testRequest);
         expect(newRequest.url).to.equal(target+path);
-    });
-
-    describe("Url matcher", () => {
-        it("should match '*' at the end", () => {
-            let test = "auth/*";
-            let url = "auth/t";
-            expect(urlMatcher(test, url)).to.eq(true);
-            url = "bauth/b";
-            expect(urlMatcher(test, url)).to.eq(false);
-        });
-
-        it("should match '*' at the beginning", () => {
-            let test = "*/auth";
-            let url = "auth/t";
-            expect(urlMatcher(test, url)).to.eq(false);
-            url = "b/auth";
-            expect(urlMatcher(test, url)).to.eq(true);
-        });
-
-        it("should match '*' in between other chars", () => {
-            let test = "users/*/auth";
-            let url = "auth/t";
-            expect(urlMatcher(test, url)).to.eq(false);
-            url = "users/b/auth";
-            expect(urlMatcher(test, url)).to.eq(true);
-        });
     });
 });
