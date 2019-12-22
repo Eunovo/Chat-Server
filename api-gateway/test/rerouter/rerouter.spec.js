790cd440373564993869ce763f5850971b4aab8f
Eunovo <eunovo9@gmail.com>
2019-12-22 21:00:31 +0100

    Remove key duplication

--- api-gateway/test/rerouter/rerouter.spec.js
+++ api-gateway/test/rerouter/rerouter.spec.js
@@ -9,7 +9,7 @@ describe("Rerouter", () => {
         const key = "/auth";
         let path = "/login";
         const test = () => {
-            let testRequest = "http://test.com/auth"+path;
+            let testRequest = "http://test.com" + key + path;
             let newRequestUrl = transformUrl(testRequest, key, target);
             expect(newRequestUrl).to.equal(target+path);
         }
