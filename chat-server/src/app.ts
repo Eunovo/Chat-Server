d276d8edca0e7041fcfa4dc666d7d1f12953fd26
Eunovo <eunovo9@gmail.com>
2019-12-25 22:46:07 +0100

    Project Setup

---
+++ chat-server/src/app.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import socketio, { Socket } from "socket.io";
+import redis from "socket.io-redis";
+
+dotenv.config();
+
+const app = express();
+const redisHost = process.env.REDIS_HOST || "localhost";
+const redisPort = parseInt(process.env.REDIS_PORT || '6379');
+export const port = process.env.PORT || 3000;
+
+let httpServer = new http.Server(app);
+let io = socketio(httpServer);
+
+if (process.env.NODE_ENV === 'production') {
+    io.adapter(redis({ host: redisHost, port: redisPort }));;
+}
+
+io.on("connection", function (client: Socket) {
+    console.log(`User, id ${client.id} connected ` +
+        `with address ${client.handshake.address}`);
+
+    client.on("PING", (data: string) => {
+        client.emit("PONG", data);
+    });
+});
+
+const server = httpServer.listen(port, function () {
+    console.log(`Listening on *:${port}`);
+});
+server.on("close", () => {
+    console.log("Server closed");
+});
+
+export default server;
---
+++ chat-server/tests/events/ping_test.spec.ts
@@ -0,0 +1,32 @@
+import client from "socket.io-client";
+import mocha from "mocha";
+import chai from "chai";
+
+import server, { port } from "../../src/app";
+
+const expect = chai.expect;
+const serverUrl = `http://localhost:${port}`;
+
+describe('WebSocker ping test', () => {
+    after((done) => {
+        server.close((err) => {
+            if (err) {
+                console.log(err);
+            }
+            done();
+        });
+    });
+
+    it('PING test', (done) => {
+        const wsClient = client(serverUrl);
+        const data = "test";
+
+        wsClient.on('connection', () => console.log('Client connected'));
+        wsClient.emit('PING', data);
+        wsClient.on('PONG', (response) => {
+            expect(response).to.equal(data);
+            wsClient.close();
+            done();
+        });
+    })
+});
