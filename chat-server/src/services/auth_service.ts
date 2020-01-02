143de6d080c59c35fbba5e6e290c5d208325bfef
Eunovo <eunovo9@gmail.com>
2020-01-02 22:13:17 +0100

    Feature: Authentication
    Install Axios
    Create Auth Service to connect with the Authentication microservice

--- chat-server/src/services/auth_service.ts
+++ chat-server/src/services/auth_service.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import { UserInfo } from "../data/chat";
 
 export default interface AuthService {
@@ -5,7 +7,28 @@ export default interface AuthService {
 }
 
 export class AuthServiceImpl implements AuthService {
-    authenticate(token: string): Promise<UserInfo> {
-        throw new Error("Method not implemented.");
+    async authenticate(token: string): Promise<UserInfo> {
+        try {
+            return this.doAuthenticate(token);
+        } catch (err) {
+            if (err.response) {
+                throw new Error(err.response.message);
+            }
+            throw new Error("Failed");
+        }
+    }
+
+    async doAuthenticate(token: string) {
+        let result = await axios.post(process.env.AUTH_SERVICE_URL, {
+            token
+        });
+        let { status, message, data } = result.data;
+        if (status === "SUCCESS") {
+            return data;
+        } else if (status === "ERROR") {
+            throw new Error(message);
+        } else {
+            throw new Error("Invalid Status Received");
+        }
     }
 }
